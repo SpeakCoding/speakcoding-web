1a9fd1aa772bb78e630acb8577a2984921df392a
azrael <azrael.25.91@gmail.com>
2021-03-04 01:31:17 +0300

    polish auth screens

--- packages/mobile/screens/auth/LogIn.jsx
+++ packages/mobile/screens/auth/LogIn.jsx
@@ -1,7 +1,6 @@
 import React, { useCallback } from 'react';
 import { Button } from '@sc/ui/mobile';
 import { useRouter } from '../../tools';
-import Logo from '../../components/logo';
 import Form from './Form';
 import s from './auth.css';
 
@@ -13,7 +12,6 @@ const LogIn = () => {
 
     return (
         <div className={s.box}>
-            <Logo />
             <div className={s.title}>Log in</div>
             <Form action='/users/authenticate.json' submitText='Log in' onSubmit={next} />
             <div className={s.message}>
--- packages/mobile/screens/auth/SignUp.jsx
+++ packages/mobile/screens/auth/SignUp.jsx
@@ -1,7 +1,6 @@
 import React, { useCallback } from 'react';
 import { Button } from '@sc/ui/mobile';
 import { useRouter } from '../../tools';
-import Logo from '../../components/logo';
 import Form from './Form';
 import s from './auth.css';
 
@@ -13,7 +12,6 @@ const SignUp = () => {
 
     return (
         <div className={s.box}>
-            <Logo />
             <div className={s.title}>Create an account</div>
             <Form action='/users.json' submitText='Next' onSubmit={next} />
             <div className={s.message}>
