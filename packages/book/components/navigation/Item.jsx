2c6907736e5c7b84e661bfd910bcd175b2b17303
azrael <azrael.25.91@gmail.com>
2021-05-17 04:15:23 +0300

    highlight current nav item

--- packages/book/components/navigation/Item.jsx
+++ packages/book/components/navigation/Item.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import pt from 'prop-types';
 import classNames from 'classnames';
+import { useLocationState } from '@sc/ui/hooks';
 import { Link } from '@sc/ui';
 import s from './navigation.css';
 
 const Item = ({ href, children }) => {
+    const [{ match }] = useLocationState({ path: href });
+
     if (!href) return <div className={classNames(s.item, s.text, s.disabled)}>{children}</div>;
 
     return (
         <div className={s.item}>
             <Link href={href}>
-                <div className={s.text}>{children}</div>
+                <div className={classNames(s.text, match && s.active)}>{children}</div>
             </Link>
         </div>
     );
