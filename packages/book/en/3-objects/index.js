4619a2cddba853754ba2f71e90a7c5e16a98c62d
azrael <azrael.25.91@gmail.com>
2021-05-23 05:01:02 +0300

    en chapter 3 - section 5

--- packages/book/en/3-objects/index.js
+++ packages/book/en/3-objects/index.js
@@ -5,6 +5,7 @@ import Part1 from './1-intro';
 import Part2 from './2-examples';
 import Part3 from './3-more-using-classes';
 import Part4 from './4-storing-objects';
+import Part5 from './5-changing-state';
 
 export default () => (
     <Page chapter={3} phone={false}>
@@ -14,5 +15,6 @@ export default () => (
         </Section>
         <Part3 />
         <Part4 />
+        <Part5 />
     </Page>
 );
