f042e89ea2d85e7835496eb7b78d3091467d20c7
azrael <azrael.25.91@gmail.com>
2021-10-10 04:50:02 +0300

    content review 12

--- packages/book/en/12-launch/index.js
+++ packages/book/en/12-launch/index.js
@@ -2,18 +2,14 @@ import React from 'react';
 import { Section } from '@sc/ui';
 import { Next } from '../../components';
 import Page from '../Page';
-import Part1 from './1-intro';
-import Part2 from './2-on-launch';
-import Part3 from './3-in-code';
-import Part4 from './4-screens';
+import Part1 from './1-on-launch';
+import Part2 from './2-screens';
 
 export default () => (
     <Page chapter={12} phone={false}>
         <Part1 />
-        <Part2 />
         <Section.Divider />
-        <Part3 />
-        <Part4 />
+        <Part2 />
         <Next href='/en/handbook'>Now to recap and reading</Next>
     </Page>
 );
