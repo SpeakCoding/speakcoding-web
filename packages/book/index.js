a624e58d925e8f163f7d58fe55bfffdadb1b470f
azrael <azrael.25.91@gmail.com>
2022-04-18 01:06:16 +0300

    update dependencies

--- packages/book/index.js
+++ packages/book/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { checkLang } from './tools/system';
 import App from './App';
 
 checkLang();
 
-render(<App />, document.getElementById('app'));
+const root = createRoot(document.getElementById('app'));
+
+root.render(<App />);
