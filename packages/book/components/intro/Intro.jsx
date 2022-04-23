a5aab1b070d63b3df9801c5252632794314a2a7e
azrael <azrael.25.91@gmail.com>
2022-04-23 23:19:57 +0300

    fix intro and review video ratio

--- packages/book/components/intro/Intro.jsx
+++ packages/book/components/intro/Intro.jsx
@@ -1,13 +1,21 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import pt from 'prop-types';
+import classNames from 'classnames';
 import { Modal } from '@sc/ui';
 import s from './intro.css';
 
+const ratio = 16 / 9;
+
 const Intro = ({ pic, video }) => {
-    const [opened, setOpened] = useState(false);
+    const [opened, setOpened] = useState(false),
+        [mode, setMode] = useState('');
 
     const open = useCallback(() => setOpened(true), []),
         close = useCallback(() => setOpened(false), []);
+
+    useEffect(() => {
+        if (opened) setMode(window.innerWidth > window.innerHeight * ratio ? 'height' : 'width');
+    }, [opened]);
 
     return (
         <>
@@ -16,12 +24,15 @@ const Intro = ({ pic, video }) => {
             </div>
 
             <Modal opened={opened} onClose={close}>
-                <div className={s.content}>
+                <div className={classNames(s.content, s[mode])}>
                     <iframe
                         allow='autoplay; fullscreen; picture-in-picture; encrypted-media;'
                         allowFullScreen
                         className={s.frame}
                         src={video}
+                        style={{
+                            aspectRatio: ratio
+                        }}
                         title='Intro video'
                     />
                 </div>
--- packages/book/components/review/Review.jsx
+++ packages/book/components/review/Review.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import pt from 'prop-types';
 import { Modal } from '@sc/ui';
 import L from '../localize';
@@ -6,63 +6,97 @@ import { useAssignment } from '../../tools';
 import Question from '../assignment/Content';
 import s from './review.css';
 
-const Review = ({ id, opened, onClose }) => {
-    const { assignment, answers } = useAssignment(id);
+const ratio = 4 / 3,
+    verticalPadding = 72 + 32 + 72; // 72px box padding top + 32px box padding bottom + 72px title size
+
+const Review = ({ id, opened }) => {
+    const { assignment, answers } = useAssignment(id),
+        [style, setStyle] = useState({}),
+        $video = useRef();
+
+    useEffect(() => {
+        if (!$video.current) return;
+        if (opened) {
+            const maxWidth = $video.current.offsetWidth,
+                maxHeight = window.innerHeight * 0.8 - verticalPadding;
+
+            setStyle(
+                maxWidth > maxHeight * ratio ? { width: 'auto', height: `${maxHeight}px` } : {}
+            );
+        }
+    }, [opened, assignment]);
 
     if (!assignment) return null;
 
     return (
-        <Modal opened={opened} onClose={onClose}>
-            <div className={s.box}>
-                <div className={s.left}>
-                    <h2 className={s.title}>{assignment.title}</h2>
-                    <div className={s.video}>
-                        <iframe
-                            allow='autoplay; fullscreen; picture-in-picture; encrypted-media;'
-                            allowFullScreen
-                            className={s.frame}
-                            src={assignment.review}
-                            title={assignment.title}
-                        />
-                    </div>
-                </div>
-                <div className={s.right}>
-                    <div className={s.content} data-context='modal-review'>
-                        {assignment.questions.map((item, i) => (
-                            <div key={item.title} className={s.item}>
-                                <Question question={item} answer={answers[i]} hideAnswer />
-                                <b>
-                                    <L lang='en'>Your answer:</L>
-                                    <L lang='ru'>Ваш ответ:</L>
-                                </b>
-                                <div className={s.answer}>
-                                    {item.ask &&
-                                        (answers[i] || (
-                                            <>
-                                                <L lang='en'>Yes</L>
-                                                <L lang='ru'>Да</L>
-                                            </>
-                                        ))}
-                                    {!item.ask && answers[i]}
-                                </div>
-                            </div>
-                        ))}
-                    </div>
+        <div className={s.box}>
+            <div className={s.left}>
+                <h2 className={s.title}>{assignment.title}</h2>
+                <div ref={$video} className={s.video}>
+                    <iframe
+                        allow='autoplay; fullscreen; picture-in-picture; encrypted-media;'
+                        allowFullScreen
+                        className={s.frame}
+                        src={assignment.review}
+                        style={{
+                            aspectRatio: ratio,
+                            ...style
+                        }}
+                        title={assignment.title}
+                    />
                 </div>
             </div>
-        </Modal>
+            <div className={s.right}>
+                <div className={s.content} data-context='modal-review'>
+                    {assignment.questions.map((item, i) => (
+                        <div key={item.title} className={s.item}>
+                            <Question question={item} answer={answers[i]} hideAnswer />
+                            <b>
+                                <L lang='en'>Your answer:</L>
+                                <L lang='ru'>Ваш ответ:</L>
+                            </b>
+                            <div className={s.answer}>
+                                {item.ask &&
+                                    (answers[i] || (
+                                        <>
+                                            <L lang='en'>Yes</L>
+                                            <L lang='ru'>Да</L>
+                                        </>
+                                    ))}
+                                {!item.ask && answers[i]}
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
     );
 };
 
 Review.propTypes = {
     id: pt.string.isRequired,
+    opened: pt.bool
+};
+
+Review.defaultProps = {
+    opened: undefined
+};
+
+const ReviewModal = ({ id, opened, onClose }) => (
+    <Modal opened={opened} onClose={onClose}>
+        <Review id={id} opened={opened} />
+    </Modal>
+);
+
+ReviewModal.propTypes = {
+    id: pt.string.isRequired,
     opened: pt.bool,
     onClose: pt.func
 };
 
-Review.defaultProps = {
+ReviewModal.defaultProps = {
     opened: undefined,
     onClose: undefined
 };
 
-export default Review;
+export default ReviewModal;
