62dcf38616af414b855cb1a46839c2b97748719c
azrael <azrael.25.91@gmail.com>
2022-11-23 10:58:54 +0300

    add grey circle for 0% progress

--- packages/book/components/header/progress/Circle.jsx
+++ packages/book/components/header/progress/Circle.jsx
@@ -1,15 +1,16 @@
 import React, { useMemo } from 'react';
 import pt from 'prop-types';
+import classNames from 'classnames';
 import { calcPath } from './utils';
 import s from './circle.css';
 
 const Circle = ({ value }) => {
-    const d = useMemo(() => calcPath(value), [value]);
+    const d = useMemo(() => calcPath(value === 0 ? 100 : value), [value]);
 
     return (
         <div className={s.box}>
             <svg
-                className={s.svg}
+                className={classNames(s.svg, value === 0 && s.empty)}
                 viewBox='0 0 64 64'
                 fill='none'
                 xmlns='http://www.w3.org/2000/svg'
